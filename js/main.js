da90ddc712ba517b4c2ce82abab9bd2360b1d4c5
madangkt <123939335+madangkt@users.noreply.github.com>
2024-06-15 14:44:31 +0800

    change some fucking text

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('UNDEFINED').split('')
+    const titles = ('This Flower is for Someone who has Gone').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
