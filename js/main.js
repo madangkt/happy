c798a334c14d88a8615192ec40bb4e618cb1a66e
madangkt <123939335+madangkt@users.noreply.github.com>
2024-08-01 23:19:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('14.25 12.15.19.20 14.5.12.9.19.1.8').split('')
+    const titles = ('i have no idea').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); 
       }
     }
 
