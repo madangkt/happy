de80af02bf9504d64b24bc48be03c6ca8edfc560
madangkt <123939335+madangkt@users.noreply.github.com>
2024-10-12 00:59:26 +0800

    ganti konsep bang

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('').split('')
+    const titles = ('Happy Birthday Yetri').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
