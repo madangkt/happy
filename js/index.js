083931689b950a918e5198549d295b59498ae8bf
madangkt <123939335+madangkt@users.noreply.github.com>
2024-10-12 01:01:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = '❤️'.split('')
+const text = 'I Have Something Special'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
