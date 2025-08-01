81d60254352c88380e917f270ba2a3dddf2f91cc
Miftahul Ramadan <123939335+madangkt@users.noreply.github.com>
2025-08-01 18:46:13 +0800

    p balap

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Yetri').split('')
+    const titles = ('For youuuu <3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
